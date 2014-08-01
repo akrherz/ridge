c8829769054863589637743b7faf28836a9290da
akrherz <akrherz@iastate.edu>
2014-08-01 07:44:36 -0500

    take un-versioned file from chinook

--- ncdc_process.py
+++ ncdc_process.py
@@ -5,24 +5,24 @@ import sys
 import time
 import subprocess
 import mx.DateTime
-prods = ['DPA','N1P','NTP']
+prods = ['TR0','TV0','N0Q','N0S','N0Z','N0U','NET']
 
 os.chdir("ncdc")
-for filegz in glob.glob("*.tar.Z"):
+for filegz in glob.glob("*.tar.gz"):
     cnt = 0
     print 'Processing', filegz
     subprocess.call("tar -xzf %s" % (filegz,), shell=True)
-    for file in glob.glob("*_*_*_201006[0-9][0-9][0-9][0-9][0-9][0-9]"):
+    for file in glob.glob("*_*_*_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"):
         parts = file.split("_") # KABR_SDUS33_PTAABR_201212231126
         # STL_20121230_2137_TV0
         newfp = "%s_%s_%s_%s" % (parts[0][1:], parts[3][:8], parts[3][8:12],
                                  parts[2][:3])
         if parts[2][:3] in prods:
-            print file, newfp
             shutil.copyfile(file, "/mesonet/ridge/input/"+ newfp)
         os.remove(file)
         cnt += 1
     while len(glob.glob("/mesonet/ridge/input/*")) > 10:
         print 'Sleeping!',
         time.sleep(30)
+    #os.remove(filegz)
     print 'did', cnt
